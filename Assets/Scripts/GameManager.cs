c8738c9c427dda11a7cf462c5f4e73104f85bfdc
stevensalex29 <stevensalex29@gmail.com>
2019-01-18 12:29:29 -0500

    Setup game loop and basic scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Game Manager contains game loop and helper functions
+ * Author: Alex Stevens
+ * Date: 1/18/19
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	//Attributes
+	private int levelNumber;
+	private Vector3 cornerLocation;
+	private Vector3 logoStartPosition;
+	private int maxBounces;
+	private GameObject logo;
+	public List<List<GameObject>> levels;
+	public int currentBounces;
+
+	// Use this for initialization
+	void Start () {
+		maxBounces = 3;
+		currentBounces = 0;
+		levelNumber = 0;
+		logo = GameObject.Find ("Logo");
+		logoStartPosition = logo.transform.position;
+		cornerLocation.x = 6.5f;
+		cornerLocation.y = 4.5f;
+		//Set level list by adding wall objects in inspector
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check if level ended/level rests/game ended (level ends if corner reached, level resets if max bounces reached, game ends if last level beat)
+		if (currentBounces == maxBounces) {
+			ResetLevel ();
+		} else if (Vector3.Distance (logo.transform.position, cornerLocation) < .5f) {
+			if (levelNumber + 1 == levels.Count) {
+				//swap to end scene
+			} else {
+				levelNumber++;
+				NextLevel ();
+			}
+		}
+	}
+
+	//Resets the level if max bounce was reached
+	void ResetLevel(){
+		//Put logo back at starting point and stop it from moving, restart current bounces
+		logo.transform.SetPositionAndRotation (logoStartPosition, Quaternion.identity);
+		logo.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		currentBounces = 0;
+	}
+		
+	//Loads the next level
+	void NextLevel(){
+		//Update starting position of logo and corner position based on level
+		switch (levelNumber) {
+			case 1:
+				//Set new logo start position and corner position
+				break;
+			case 2:
+				//Set new logo start position and corner position
+				break;
+		}
+
+		//Get walls from new current level list, add to wall list, and set as active in scene
+		GameObject wallM = GameObject.Find ("WallManager");
+		wallM.GetComponent<WallManager> ().ClearWalls ();
+		for (int i = 0; i < levels [levelNumber].Count; i++) {
+			wallM.GetComponent<WallManager> ().AddWalls (levels [levelNumber] [i]);
+		}
+
+	}
+}
---
+++ Assets/Scripts/WallManager.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Manages collision and other operations for walls
+ * Author: Alex Stevens
+ * Date: 1/18/19
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour {
+	//Attributes
+	public List<GameObject> walls;
+
+	// Use this for initialization
+	void Start () {
+		//Occupy walls list with first level walls, set as active game objects as well
+		walls = new List<GameObject>();
+		/*List<GameObject> firstLev = GameObject.Find("GameManager").GetComponent<GameManager>().levels[0];
+		for (int i = 0; i < firstLev.Count; i++) {
+			firstLev [i].SetActive (true);
+			walls.Add (firstLev [i]);
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check for collision of logo and walls, do collision
+	}
+
+	//Clear walls from list
+	public void ClearWalls(){
+		//Set removed walls as not active
+		for (int i = 0; i < walls.Count; i++) {
+			walls [i].SetActive (false);
+		}
+		walls.Clear ();
+	}
+
+	//Add wall to list
+	public void AddWalls(GameObject wall){
+		//Set added walls as active
+		wall.SetActive (true); 
+		walls.Add (wall);
+	}
+}
