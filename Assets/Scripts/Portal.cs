2c85165e3fc3fd226e37ed155e32030896f74aca
stevensalex29 <stevensalex29@gmail.com>
2019-01-23 10:47:33 -0500

    added portal script

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    //Attributes
+    private Vector3 direction;
+    public GameObject portalLink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
