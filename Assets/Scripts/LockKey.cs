3e20377b75bc9deabce31bbf3a0906a478926354
Jake <jxt8029@rit.edu>
2019-01-27 12:15:25 -0500

    added key, lock mechanic

---
+++ Assets/Scripts/LockKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockKey : MonoBehaviour
+{
+    public GameObject lockedWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(lockedWall != null && coll.gameObject.name == "DVDLogo")
+        {
+            GetComponent<SpriteRenderer>().color = Color.black;
+            Destroy(lockedWall);
+        }
+    }
+}
