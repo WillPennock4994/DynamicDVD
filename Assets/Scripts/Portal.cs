fc7c0350bdf7c18d00b14e159ac395842b7a8358
Duncan Lord <dxl1435@g.rit.edu>
2019-02-03 16:52:55 -0500

    Fixed Portals, now works in all directions, and can have multiple sets per scene.  Requires adding tags to portals to determine direction portal is facing

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -19,7 +19,7 @@ public class Portal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,11 +27,33 @@ public class Portal : MonoBehaviour
         if(collision.gameObject.name == "DVDLogo")
         {
             //translate to the position of the other portal, then place the logo in front of the new portal's up vector (right in front of it)
-            logo.transform.Translate(lengthToPortal - portalLink.transform.up * 1.5f);
+            logo.transform.Translate(lengthToPortal - portalLink.transform.up);
 
             // make logo go in same direction as portal is placed, then rotate logo back to its default rotation to avoid logo flipping issue
-            logo.GetComponent<Rigidbody2D>().velocity = logo.GetComponent<Rigidbody2D>().velocity * -transform.up;
+            // logo.GetComponent<Rigidbody2D>().velocity *= new Vector2(portalLink.transform.up.x, portalLink.transform.up.y);
             //logo.transform.rotation = Quaternion.Euler(new Vector3());
+            // Debug.Log(portalLink.tag);
+
+            // if portal is facing up
+            if (portalLink.tag == "upPortal") {
+                logo.transform.Translate(new Vector3( 0, 1, 0));
+                logo.GetComponent<Rigidbody2D>().velocity = new Vector2(0, Mathf.Max(Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.x), Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.y)));
+            }
+            // if portal is facing down
+            if (portalLink.tag == "downPortal"){
+                logo.transform.Translate(new Vector3(0, -1, 0));
+                logo.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -Mathf.Max(Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.x), Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.y)));
+            }
+            // if portal is facing right
+            if (portalLink.tag == "rightPortal"){
+                logo.transform.Translate(new Vector3(1, 0, 0));
+                logo.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Max(Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.x), Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.y)), 0);
+            }
+            // if portal is facing left
+            if (portalLink.tag == "leftPortal"){
+                logo.transform.Translate(new Vector3(-1, 0, 0));
+                logo.GetComponent<Rigidbody2D>().velocity = new Vector2(-Mathf.Max(Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.x), Mathf.Abs(logo.GetComponent<Rigidbody2D>().velocity.y)), 0);
+            }
         }
     }
 }
