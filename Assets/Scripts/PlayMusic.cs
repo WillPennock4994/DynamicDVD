466784cf6dd353af5e31f7a6c8f71baba3387e41
Jake <jxt8029@rit.edu>
2019-02-06 17:05:07 -0500

    fixed audio duplication issue, needs rebuild

--- Assets/Scripts/PlayMusic.cs
+++ Assets/Scripts/PlayMusic.cs
@@ -4,34 +4,25 @@ using UnityEngine;
 
 public class PlayMusic : MonoBehaviour
 {
-    private AudioSource audio;
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-        audio = GetComponent<AudioSource>();
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayAudio();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    public void PlayAudio()
-    {
-        if (audio.isPlaying) return;
-        audio.Play();
-    }
-
-    public void StopAudio()
-    {
-        audio.Stop();
     }
 }
