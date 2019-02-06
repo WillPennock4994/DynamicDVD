0e4e1045fae964f9a911227e7124ea7a25dc8fc9
Jake <jxt8029@rit.edu>
2019-02-06 16:37:39 -0500

    added bg music (plays in win scene, but isn't really noticable

---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayAudio();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayAudio()
+    {
+        if (audio.isPlaying) return;
+        audio.Play();
+    }
+
+    public void StopAudio()
+    {
+        audio.Stop();
+    }
+}
